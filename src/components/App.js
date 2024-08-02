e6ff87a4557185184fec82768738020455d201b4
Prashant Kumar <prashant.aman9@gmail.com>
2024-08-02 13:41:34 +0530

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const App = () => {
-  return (
-    <div id="main">
-               {/* Do not remove the main div */}
-    </div>
-  )
+	const relativeList = ['Shyam', 'Ram', 'Hari', 'Sita']
+	return (
+		<div id='main'>
+			{/* Do not remove the main div */}
+			<ul key='relativeList'>
+				{relativeList.map((relative, index) => (
+					<li key={`relativeListItem${index + 1}`}>{relative}</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default App
